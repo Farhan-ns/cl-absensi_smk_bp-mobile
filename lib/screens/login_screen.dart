8466f975d7eebc07452457afb4bc253060e42df1
Farhanunnasih <farhanunnasih@gmail.com>
2023-07-10 11:09:17 +0700

    Make screen scrollable

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -15,101 +15,100 @@ class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      resizeToAvoidBottomInset: false,
       body: SafeArea(
-        child: Column(
-          children: [
-            // Container(
-            //   // height: 170,
-            //   color: Colors.grey.shade400,
-            //   child: ),
-            // ),
-            Image.asset(
-              'assets/images/login-banner.jpg',
-              height: 250,
-              fit: BoxFit.fill,
-            ),
-            const SizedBox(height: 25),
-            const Image(
-              height: 90,
-              image: AssetImage('assets/icons/logo-bp.png'),
-            ),
-            const SizedBox(height: 10),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  'LOGIN',
-                  style: Theme.of(context).textTheme.titleMedium?.copyWith(
-                        color: const Color(0xFF707070),
-                        fontWeight: FontWeight.bold,
-                      ),
-                ),
-              ],
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 25),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Image.asset(
+                'assets/images/login-banner.jpg',
+                height: 250,
+                fit: BoxFit.fill,
+              ),
+              const SizedBox(height: 25),
+              const Image(
+                height: 95,
+                image: AssetImage('assets/icons/logo-bp.png'),
+              ),
+              const SizedBox(height: 10),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   Text(
-                    'Username',
-                    style: Theme.of(context).textTheme.labelSmall,
-                  ),
-                  const SizedBox(height: 5),
-                  MyTextField(
-                    textInputType: TextInputType.emailAddress,
-                    controller: controller.emailTxtController,
-                  ),
-                  const SizedBox(height: 15),
-                  Text(
-                    'Password',
-                    style: Theme.of(context).textTheme.labelSmall,
-                  ),
-                  const SizedBox(height: 5),
-                  MyTextField(
-                    textInputType: TextInputType.emailAddress,
-                    controller: controller.passwordTxtController,
-                  ),
-                  const SizedBox(height: 15),
-                  Row(
-                    children: [
-                      Expanded(
-                        child: MyElevatedButton(
-                          context,
-                          Obx(
-                            () => controller.isSubmitting.value
-                                ? const ButtonCircularProgress()
-                                : const ButtonLabel('LOGIN'),
-                          ),
-                          onPressed: () async {
-                            var success = await controller.attemptLogin();
-                            if (success && context.mounted) context.go('/home');
-                          },
+                    'LOGIN',
+                    style: Theme.of(context).textTheme.titleMedium?.copyWith(
+                          color: const Color(0xFF707070),
+                          fontWeight: FontWeight.bold,
                         ),
-                      ),
-                    ],
                   ),
                 ],
               ),
-            ),
-            Expanded(
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.only(top: 30),
-                    child: Transform.flip(
-                      flipX: true,
-                      child: const Image(
-                        image: AssetImage('assets/images/vector-3.png'),
-                      ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Username',
+                      style: Theme.of(context).textTheme.labelSmall,
                     ),
-                  ),
-                ],
+                    const SizedBox(height: 5),
+                    MyTextField(
+                      textInputType: TextInputType.emailAddress,
+                      controller: controller.emailTxtController,
+                    ),
+                    const SizedBox(height: 15),
+                    Text(
+                      'Password',
+                      style: Theme.of(context).textTheme.labelSmall,
+                    ),
+                    const SizedBox(height: 5),
+                    MyTextField(
+                      textInputType: TextInputType.emailAddress,
+                      controller: controller.passwordTxtController,
+                    ),
+                    const SizedBox(height: 15),
+                    Row(
+                      children: [
+                        Expanded(
+                          child: MyElevatedButton(
+                            context,
+                            Obx(
+                              () => controller.isSubmitting.value
+                                  ? const ButtonCircularProgress()
+                                  : const ButtonLabel('LOGIN'),
+                            ),
+                            onPressed: () async {
+                              var success = await controller.attemptLogin();
+                              if (success && context.mounted)
+                                context.go('/home');
+                            },
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
               ),
-            ),
-          ],
+              // FittedBox(
+              //   fit: BoxFit.fitWidth,
+              //   child: Row(
+              //     mainAxisAlignment: MainAxisAlignment.end,
+              //     children: [
+              //       Padding(
+              //         padding: const EdgeInsets.only(top: 30),
+              //         child: Transform.flip(
+              //           flipX: true,
+              //           child: const Image(
+              //             image: AssetImage('assets/images/vector-3.png'),
+              //             height: 150,
+              //           ),
+              //         ),
+              //       ),
+              //     ],
+              //   ),
+              // ),
+            ],
+          ),
         ),
       ),
     );
